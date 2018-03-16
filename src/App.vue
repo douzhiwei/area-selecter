6651b9f922d30cf503848fbddcdced018c601cbb
douzhiwei <douzhiwei@zmeng123.com>
2018-03-16 15:40:34 +0800

    add /dist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="app">
-        <area-select ref="areaSelect" :areaRes="areaRes" @res-change="areaResChange"></area-select>
+        <div style="overflow: hidden">
+            <area-select ref="areaSelect" :areaRes="areaRes" @res-change="areaResChange"></area-select>
+        </div>
+        <div>
+            <p style="color: cornflowerblue">已选择的城市id</p>
+            <p>{{selectList}}</p>
+        </div>
+
     </div>
 </template>
 
@@ -10,12 +17,13 @@
         name: 'app',
         data() {
             return {
-                areaRes: [ "120000", "130000", "140105", "140106", "140107", "140108"]
+                areaRes: [ "120000", "130000", "140105", "140106", "140107", "140108"],
+                selectList: []
             }
         },
         methods: {
             areaResChange(val) {
-
+                this.selectList = val
             }
         },
         mounted() {
