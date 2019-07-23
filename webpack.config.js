0ee215d6f3d85cc53ba5f526b2f05f913185cfbf
douzhiwei <douzhiwei@zmeng123.com>
2019-07-23 16:31:24 +0800

    打包文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  // entry: './src/main.js', // 开发时项目入口
   entry: './src/lib/index.js', // 打包发布时入口
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'area-selecter.js', // 我们可不想打包后叫build 多low啊 起一个与项目相对应的
-    library: 'areaSelect', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
-    libraryTarget: 'umd', //libraryTarget会生成不同umd的代码，例如可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
-    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+    filename: 'area-selecter.js',
+    library: 'areaSelect', // library指定的就是你使用require时的模块名，这里便是require("areaSelect")
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [{
